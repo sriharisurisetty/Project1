502ab4073f73fb1d4b42d459f26f04d4cb85b143
srsur1 <srihari.surisetty1@inter.ikea.com>
2025-10-04 09:01:18 +0200

    first

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const getMessage = (): string => {
     const message: string = "Hello, TypeScript!";
     return message;
-};
+}; 
 
 console.log("This is a TypeScript project.");
 console.log(getMessage());
