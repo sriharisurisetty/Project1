0c830c4d9b5005f4b5b6052761cdbab8813c6d41
Srihari <srivisw@gmail.com>
2025-10-04 22:15:33 +0200

    commit

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,36 @@ const getMessage = (): string => {
 
 console.log("This is a TypeScript project.");
 console.log(getMessage());
+
+
+
+import express from "express";
+
+const app = express();
+
+app.get("/hello", (req: express.Request, res: express.Response) => {
+  res.send(`
+    <p>Hello Srihari, click on the button on below</p>
+    <button id="apiBtn">Call API</button>
+     <p id="result"></p>
+     <script>
+      document.getElementById("apiBtn").addEventListener("click", async () => {
+        try {
+          const response = await fetch("/welcome");
+          const data = await response.json();
+          document.getElementById("result").innerText = data.message;
+        } catch (err) {
+          document.getElementById("result").innerText = "Error calling API";
+        }
+      });
+    </script>
+  `);
+});
+
+app.get("/welcome", (req: express.Request, res: express.Response) => {
+  res.send("Welcome to the world of JS/TS");
+});
+
+app.listen(3001, () => {
+  console.log("Server running at http://localhost:3001");
+});
